c325ff9386b124b3f3cc79e7b6fe10d30d579e59
alex-dishen <102871422+alex-dishen@users.noreply.github.com>
2023-02-08 11:52:57 +0200

    Create Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,35 @@
+import { Link } from 'react-router-dom';
+import {
+  StyledHome,
+  Greeting,
+  NavigationSection,
+  Option,
+  Text,
+  Underline,
+} from 'pages/Home/styles';
+
 function Home() {
-  return <div />;
+  return (
+    <StyledHome>
+      <NavigationSection>
+        <Option>
+          <span>A</span> I heard you are designing great apps & websites!
+        </Option>
+        <Option>
+          <span>B</span> I like reading about design & technology!
+        </Option>
+        <Option>
+          <span>C</span> Oleksandr, who?!
+        </Option>
+      </NavigationSection>
+      <Text>
+        Never Mind -
+        <Link to="/">
+          Just Say Hi <Underline />
+        </Link>
+      </Text>
+    </StyledHome>
+  );
 }
 
 export default Home;
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,5 +1,13 @@
 import * as styled from 'styled-components';
 
-const GlobalStyle = styled.createGlobalStyle``;
+const GlobalStyle = styled.createGlobalStyle`
+  #root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+    background-color: rgb(13, 13, 13);
+  }
+`;
 
 export default GlobalStyle;
