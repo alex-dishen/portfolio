7875abfd268147a6ded9e87be17854d613a8399c
Oleksandr <didyshen.oleksandr@gmail.com>
2023-03-12 19:01:01 +0200

    Add link to my blog

---
+++ src/pages/Home/constants.ts
@@ -0,0 +1,17 @@
+export const options = [
+  {
+    page: '/projects',
+    variant: 'A',
+    text: 'I heard you are creating great websites!',
+  },
+  {
+    page: 'https://medium.com/@oleksandrdidyshen',
+    variant: 'B',
+    text: 'I like reading about technologies!',
+  },
+  {
+    page: '/about',
+    variant: 'C',
+    text: 'Oleksandr, who?!',
+  },
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { ReactComponent as RightArrow } from 'assets/ui/arrow-right.svg';
+import { options } from 'pages/Home/constants';
 import {
   StyledHome,
   Greeting,
@@ -10,6 +10,7 @@ import {
   Underline,
 } from 'pages/Home/styles';
 import meInBlueShirt from 'assets/black-shirt.JPG';
+import { ReactComponent as RightArrow } from 'assets/ui/arrow-right.svg';
 
 function Home() {
   return (
@@ -18,31 +19,21 @@ function Home() {
         <h1>
           Hey, I'm
           <ImageHolder>
-            <img src={meInBlueShirt} alt="" />
+            <img src={meInBlueShirt} alt="Oleksandr in blue shirt" />
           </ImageHolder>
           Oleksandr.
         </h1>
         <h1>A Front-End Developer</h1>
       </Greeting>
       <NavigationSection>
-        <Option to="/projects">
-          <p>
-            <span>A</span> I heard you are creating great websites!
-          </p>
-          <RightArrow />
-        </Option>
-        {/* <Option to="">
-          <p>
-            <span>B</span> I like reading about technologies!
-          </p>
-          <RightArrow />
-        </Option> */}
-        <Option to="/about">
-          <p>
-            <span>C</span> Oleksandr, who?!
-          </p>
-          <RightArrow />
-        </Option>
+        {options.map((option) => (
+          <Option to={option.page}>
+            <p>
+              <span>{option.variant}</span> {option.text}
+            </p>
+            <RightArrow />
+          </Option>
+        ))}
       </NavigationSection>
       <Text>
         Never Mind -
