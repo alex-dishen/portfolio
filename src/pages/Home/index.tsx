ff9f0208f3f69808c479c929bbe09a66bf26af17
Oleksandr <didyshen.oleksandr@gmail.com>
2023-05-11 19:53:51 +0300

    Test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,7 +20,7 @@ const Home = () => (
   <StyledHome>
     <Welcome>
       <TopGreeting>
-        <Greeting>Hey, I'm Branch for Testing 2</Greeting>
+        <Greeting>Hey, I'm Branch for Testing 5</Greeting>
         <ImageHolder>
           <img src={meInBlueShirt} alt="Oleksandr in black shirt" />
         </ImageHolder>
