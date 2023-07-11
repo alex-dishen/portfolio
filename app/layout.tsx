bd955a4bf9c9d6b37521704fb9baeeb8ba123a79
Oleksandr <didyshen.oleksandr@gmail.com>
2023-07-11 20:57:09 +0300

    Remove font and improve metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
 import { ReactNode } from 'react'
 import { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
 import StyledComponentsRegistry from '../lib/registry'
 import '../src/styles/normalize.css'
-import '../src/styles/font.css'
 import Navbar from 'src/components/Navbar'
+
+const manrope = Manrope({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
   title: 'Oleksandr Didyshen',
-  description: 'This is my portfolio',
+  description:
+    "I'm a Software Engineer with 22+ finished projects. You are welcome to see my creations",
+  keywords:
+    'Portfolio, Front-End Developer, Full-Stack Developer, React, Next.js, TypeScript, Node.js, Express, PostgreSQL',
+  openGraph: {
+    title: 'Oleksandr Didyshen',
+    images: ['../images/home-page.png'],
+  },
 }
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" className={manrope.className}>
       <body>
         <StyledComponentsRegistry>
           <Navbar />
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,7 +6,9 @@ const GlobalStyle = styled.createGlobalStyle`
   }
 
   body {
-    font-family: Manrope, sans-serif;
+    font-family: Manrope, system-ui, -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+      'Helvetica Neue';
     display: flex;
     flex-direction: column;
     align-items: center;
